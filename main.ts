450249f03d856f5535fe474036c5f0ea17dd4648
dafeng306 <75168543+dafeng306@users.noreply.github.com>
2020-12-02 09:32:54 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+if (true) {
+    basic.showNumber(input.lightLevel())
+    music.playMelody("G A G A G A G A ", 120)
+    music.ringTone(349)
+    music.ringTone(262)
+}
+basic.forever(function () {
+	
+})
